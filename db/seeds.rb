e507281bacf702e2cd03bd8e0ea9cb84db0e53a5
Apama Jamshidi <apama.jamshidi@gmail.com>
2020-02-20 13:43:50 -0800

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.create(title: 'Hello world',content: 'lorem ipso',slug: 'hello-world')
+
+User.create(name: "apama",email: "a@a.com",password: "abc")
+Review.create(review_text: "it was really nice", res_id: 16618773, user_id: 1)
