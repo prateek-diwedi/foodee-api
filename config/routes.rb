6353138897e30a43a721ba2b327355adceb8b73c
Apama Jamshidi <apama.jamshidi@gmail.com>
2020-02-20 11:07:01 -0800

    Adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+
+  resources :articles
+  end
+
+resource :session, only: [:new, :create, :destroy]
+resources :users
+resources :post_maps, only: [:index]
+
+root "app#index"
+
+
+resources :posts  do
+  resources :favourites, only: [:create, :destroy]
+  resources :reviews, only: [:create, :destroy]
+  resources :rating, only: [:create, :destroy]
+
 end
+
+
+end
+
+
+
